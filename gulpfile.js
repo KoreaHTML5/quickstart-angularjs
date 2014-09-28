f952f0f3415b7a425ebad207188156951c46f3da
ragingwind <ragingwind@gmail.com>
2014-09-28 21:27:25 +0900

    init

---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+/**
+ *
+ *  Quickstart gulpfile.js, This file based on Web Starter Kit
+ *
+ *  Licensed under Apache License, Copyright 2014 Google Inc.
+ *  See the LICENSE for more details of licence
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// Lint JavaScript
+gulp.task('jshint', function () {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe(reload({stream: true, once: true}))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+});
+
+// Optimize Images
+gulp.task('images', function () {
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size({title: 'images'}));
+});
+
+// Copy All Files At The Root Level (app)
+gulp.task('copy', function () {
+  return gulp.src([
+    'app/*',
+    '!app/*.html',
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'copy'}));
+});
+
+// Scan Your HTML For Assets & Optimize Them
+gulp.task('html', function () {
+  var assets = $.useref.assets({searchPath: ['app']});
+
+  return gulp.src('app/**/*.html').on('error', console.error.bind(console))
+    .pipe(assets)
+    .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
+    .pipe($.if('*.css', $.csso()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe($.if('*.html', $.minifyHtml()))
+    .pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'html'}));
+});
+
+// Copy the files associated with components
+gulp.task('assets', function () {
+  return gulp.src([
+    'bower_components/iCheck/skins/**/*',
+    '!bower_components/iCheck/skins/all.css',
+    'bower_components/chosen/chosen-sprite@2x.png',
+    'bower_components/chosen/chosen-sprite.png',
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist/styles'))
+    .pipe($.size({title: 'copy'}));
+});
+
+// Clean Output Directory
+gulp.task('clean', del.bind(null, ['dist']));
+
+// Watch Files For Changes & Reload
+gulp.task('serve', function () {
+  browserSync({
+    notify: false,
+    logPrefix: 'QS',
+    server: {
+      baseDir: 'app',
+      routes: {
+        "/bower_components": "./bower_components"
+      }
+    }
+  });
+
+  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/styles/**/*.css'], [reload]);
+  gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+  gulp.watch(['app/images/**/*'], reload);
+});
+
+// Build and serve the output from the dist build
+gulp.task('serve:dist', ['default'], function () {
+  browserSync({
+    notify: false,
+    logPrefix: 'QS',
+    server: 'dist'
+  });
+});
+
+// Build Production Files, the Default Task
+gulp.task('default', ['clean'], function (cb) {
+  runSequence(['jshint', 'html', 'images', 'copy', 'assets'], cb);
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Quickstart for Angularjs
+
+# CONTRIBUTE
+
+See [CONTRIBUTE](https://github.com/KoreaHTML5/dev.koreahtml5.kr/blob/master/CONTRIBUTE.md) for more details
+
+# LICENSE
+
+See LICENSE for more details
